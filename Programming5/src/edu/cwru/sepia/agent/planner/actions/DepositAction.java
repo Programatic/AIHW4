8df0094b87a158d5c2424e1c9e3142ec8e5b7d6a
Ford Smith <fms34@case.edu>
2021-04-24 00:09:17 -0400

    deposit

--- Programming5/src/edu/cwru/sepia/agent/planner/actions/DepositAction.java
+++ Programming5/src/edu/cwru/sepia/agent/planner/actions/DepositAction.java
@@ -5,17 +5,16 @@ import edu.cwru.sepia.agent.planner.Peasant;
 import edu.cwru.sepia.agent.planner.Position;
 
 public class DepositAction implements StripsAction {
-	int peasantId;
-	Position peasantPos;
-	Position townHallPos = GameState.TOWN_HALL_POSITION;
-	boolean hasResource;
-	
+	private final int peasantId;
+	private final Position peasantPos, townHallPos = GameState.TOWN_HALL_POSITION;
+	private final boolean hasResource;
+
 	public DepositAction(Peasant peasant) {
 		this.peasantId = peasant.getId();
 		this.peasantPos = peasant.getPosition();
 		this.hasResource = peasant.isCarrying();
 	}
-	
+
 	@Override
 	public boolean preconditionsMet(GameState state) {
 		return hasResource && peasantPos.equals(townHallPos);
@@ -29,13 +28,12 @@ public class DepositAction implements StripsAction {
 	}
 
 	@Override
+	public int getPeasantID() {
+		return peasantId;
+	}
+
+	@Override
 	public Position getPosition() {
 		return townHallPos;
 	}
-	
-	@Override
-	public int getPeasantID() {
-		return peasantId;	
-	}
-
 }
