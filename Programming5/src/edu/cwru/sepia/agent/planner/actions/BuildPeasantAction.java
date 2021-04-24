4b57b1ace3e10aad152ca91cc7affd2022d8600f
Alexander Bradley <ach159@case.edu>
2021-04-24 18:25:30 -0400

    ach159: Updated BuildPeasantAction

--- Programming5/src/edu/cwru/sepia/agent/planner/actions/BuildPeasantAction.java
+++ Programming5/src/edu/cwru/sepia/agent/planner/actions/BuildPeasantAction.java
@@ -3,38 +3,31 @@ package edu.cwru.sepia.agent.planner.actions;
 import edu.cwru.sepia.agent.planner.GameState;
 import edu.cwru.sepia.agent.planner.Position;
 
-public class BuildPeasantAction implements  StripsAction {
-    private int townGold, townFood;
+public class BuildPeasantAction implements StripsAction {
 
-    public BuildPeasantAction(int townGold, int townFood) {
-        this.townFood = townFood;
-        this.townGold = townGold;
-    }
+	@Override
+	public boolean preconditionsMet(GameState state) {
+		return state.canBuild();
+	}
 
-    @Override
-    public boolean preconditionsMet(GameState state) {
-        return townFood >= 0 && townGold >= GameState.BUILD_GOLD_NEEDED;
-    }
+	@Override
+	public void apply(GameState state) {
+		state.applyBuildPeasantAction();
+		state.update(this);
+	}
 
-    @Override
-    public GameState apply(GameState state) {
-        state.applyBuildAction();
-        state.update(this);
-        return state;
-    }
+	@Override
+	public int getPeasantID() {
+		return GameState.TOWN_HALL_ID;
+	}
 
-    @Override
-    public int getPeasantID() {
-        return 0;
-    }
+	@Override
+	public Position getPosition() {
+	    return null;
+	}
 
-    @Override
-    public Position getPosition() {
-        return null;
-    }
-
-    @Override
-    public double getCost() {
-        return 1;
-    }
+	@Override
+	public double getCost() {
+		return 1;
+	}
 }
